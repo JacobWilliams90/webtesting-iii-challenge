5d8c9ea9367af36602400c053aa12a700699f919
Jacob Williams <JaWilliamsDev@gmail.com>
2019-10-30 06:21:14 -0400

    inital commit with First test (Disabled buttons) completed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,25 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import Controls from "./Controls";
+
+describe("<Controls />", () => {
+  it("Is open and unlocked", () => {
+    const mock = jest.fn();
+
+    const { queryByText } = render(
+      <Controls locked={false} closed={false} toggleClosed={mock} />
+    );
+
+    const lockButton = queryByText("Lock Gate");
+    expect(lockButton.disabled).toBe(true);
+
+    const closeButton = queryByText("Close Gate");
+    expect(closeButton.disabled).toBe(false);
+
+    fireEvent.click(closeButton);
+
+    expect(mock).toBeCalled();
+  });
+});
